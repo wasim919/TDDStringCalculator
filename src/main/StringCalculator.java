e15320f5b011f6840751e3d88abbe0b0dadbb3cf
wasim919 <wasimwik919@gmail.com>
2020-06-04 16:42:39 +0530

    handleEmptyString test done

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -4,4 +4,7 @@ package main;
  * Created by wasim on 04-06-2020.
  */
 public class StringCalculator {
+    public int sum(String s) {
+        return 0;
+    }
 }
--- src/test/StringCalculatorTest.java
+++ src/test/StringCalculatorTest.java
@@ -1,7 +1,25 @@
 package test;
 
+import main.StringCalculator;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by wasim on 04-06-2020.
  */
 public class StringCalculatorTest {
+    private StringCalculator stringCalculator;
+
+    @Before
+    public void init() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    public void emptyStringSumsTo0() {
+        init();
+        assertEquals(stringCalculator.sum(""), 0);
+    }
 }
